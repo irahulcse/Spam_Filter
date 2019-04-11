5a6768ae78589da01128564990d7123fdae07b9a
Rahul Chandra <1rahulchandra1@gmail.com>
2019-04-11 08:30:46 +0530

    Adding the Project Report and Research Paper(For Fun):)

--- spam_filter.py
+++ spam_filter.py
@@ -1,5 +1,5 @@
-
-#%% [markdown]
+#%%
+#%%
 # # Spam Filter In Python3 and Python4
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
@@ -13,35 +13,35 @@ import tkinter
 from tkinter import messagebox
 #%matplotlib inline
 
-#%% [markdown]
+#%% 
 # # loading the dataset in the mails
 mails=pd.read_csv('spam.csv',encoding='latin-1')
 mails.head()
-#%% [markdown]
+#%% 
 # # now we will be going to drop the different colums from the datset so that we can use it
 mails.drop(['Unnamed: 2','Unnamed: 3','Unnamed: 4'],axis=1,inplace=True)
 mails.head()
 
 #now we going to rename the columsn so that we would be ablet to find the data
-#%% [markdown]
+#%% 
 # # Renaming of the Columns to make it more meaningful
 mails.rename(columns={'v1':'labels','v2':'message'},inplace=True)
 mails.head()
 
-#%% [markdown]
+#%% 
 # # now we will be going to count the mails according to the labels
 mails['labels'].value_counts()
 
 
-#%% [markdown]
+#%% 
 mails['label']=mails['labels'].map({'ham':0,'spam':1})
 mails.head()
 
-#%% [markdown]
+#%% 
 mails.drop(['labels'],axis=1,inplace=True)
 mails.head()
 
-#%% [markdown]
+#%% 
 totalMails=4825+747
 trainIndex,testIndex=list(),list()
 for i in range(mails.shape[0]):
@@ -56,15 +56,15 @@ trainData.reset_index(inplace=True)
 trainData.drop(['index'],axis=1,inplace=True)
 trainData.head()
 
-#%% [markdown]
+#%% 
 testData.reset_index(inplace=True)
 testData.drop('index',axis=1,inplace=True)
 testData.head()
-#%% [markdown]
+#%% 
 trainData['label'].value_counts()
-#%% [markdown]
+#%% 
 testData['label'].value_counts()
-#%% [markdown]
+#%% 
 spam_words=''.join(list(mails[mails['label']==1]['message']))
 spam_wc=WordCloud(width=512,height=512).generate(spam_words)
 plt.figure(figsize=(10,8),facecolor='k')
@@ -72,7 +72,7 @@ plt.imshow(spam_wc)
 plt.axis('off')
 plt.tight_layout(pad=0)
 plt.show()
-#%% [markdown]
+#%% 
 
 #doing the same thing for the ham words too
 ham_words=''.join(list(mails[mails['label']==0]['message']))
@@ -82,10 +82,10 @@ plt.imshow(ham_wc)
 plt.axis('off')
 plt.tight_layout(pad=0)
 plt.show()
-#%% [markdown]
+#%% 
 trainData.head()
 trainData['label'].value_counts()
-#%% [markdown]
+#%% 
 testData.head()
 testData['label'].value_counts()
 def process_message(message, lower_case = True, stem = True, stop_words = True, gram = 2):
@@ -105,7 +105,7 @@ def process_message(message, lower_case = True, stem = True, stop_words = True, 
         stemmer = PorterStemmer()
         words = [stemmer.stem(word) for word in words]   
     return words
-#%% [markdown]
+#%% 
 # # Main Function For Training the model(class SpamClassifier(Object))
 class SpamClassifier(object):
     def __init__(self, trainData, method = 'tf-idf'):
@@ -210,7 +210,7 @@ class SpamClassifier(object):
             result[i] = int(self.classify(processed_message))
         return result
     
-#%% [markdown]
+#%% 
 def metrics(labels, predictions):
     true_pos, true_neg, false_pos, false_neg = 0, 0, 0, 0
     for i in range(len(labels)):
@@ -230,7 +230,7 @@ def metrics(labels, predictions):
 
 root = tkinter.Tk()
 root.withdraw()
-#%% [markdown]
+#%% 
 # # Tkinter based Message Dialog
 def alldonewithflyingcolors(pm,new):
     if sc_tf_idf.classify(pm)==True:
@@ -244,54 +244,79 @@ def alldonewithflyingcolors(pm,new):
         messagebox.showwarning("Message Passed to Spam Filter Model",message=new)
         messagebox.showinfo("Good News",message="Answer==Good Email")
         #messagebox.showinfo("Message Passed to Spam Filter is",message="Message Passed=="+new+"\nAnwer==Good Email")
-#%% [markdown]
+#%% 
 
 
-#%% [markdown]
+#%% 
 # # Training the Datasets through the SpamClassifier method
 sc_tf_idf=SpamClassifier(trainData,'tf-idf')
 sc_tf_idf.train()
 preds_tf_idf=sc_tf_idf.predict(testData['message'])
 metrics(testData['label'],preds_tf_idf)
-#%% [markdown]
+#%% 
 sc_bow=SpamClassifier(trainData,'bow')
 sc_bow.train()
 preds_bow=sc_bow.predict(testData['message'])
 metrics(testData['label'],preds_bow)
-#%% [markdown]
+#%% 
 # # Testing on the some problems
 new='I cant pick the phone'
 pm=process_message(new)
-print("The new message in the dataset"+pm)
+print(pm)
 #print(new)
 #messagebox.showerror("Gonna to Check this Prototype",new)
 alldonewithflyingcolors(pm,new)
-#%% [markdown]
+#%% 
 new='Congratulations you are awarded $500'
 pm=process_message(new)
 print(pm)
 alldonewithflyingcolors(pm,new)
-#%% [markdown]
+#%% 
 new='Its is a new spam message'
 pm=process_message(new)
 alldonewithflyingcolors(pm,new)
-#%% [markdown]
+#%% 
 new='Thanks for your subscription to Ringtone UK your mobile will be charged �5/month Please confirm by replying YES or NO. If you reply NO you will not be charged,,,'
 pm=process_message(new)
 alldonewithflyingcolors(pm,new)
-#%% [markdown]
+#%% 
 new='This msg is for your mobile content order It has been resent as previous attempt failed due to network error Queries to customersqueries@netvision.uk.com,,,'
 pm=process_message(new)
 alldonewithflyingcolors(pm,new)
-#%% [markdown]
+#%% 
 new='Money reward $500'
 pm=process_message(new)
 alldonewithflyingcolors(pm,new)
-#%% [markdown]
+#%% 
 new='I am giving you 500$'
 pm=process_message(new)
 alldonewithflyingcolors(pm,new)
-#%% [markdown]
+#%% 
 new='I am Rahul Chandra going to give you 500$ as a credit '
+pm=process_message(new)
+alldonewithflyingcolors(pm,new)
+
+#%%
+new='Hello'
+pm=process_message(new)
+alldonewithflyingcolors(pm,new)
+
+#%%
+new='Aaayushi will be going to go to the home next week'
+pm=process_message(new)
+alldonewithflyingcolors(pm,new)
+
+#%%
+new='Got $500 as a gift coupon'
+pm=process_message(new)
+alldonewithflyingcolors(pm,new)
+
+#%%
+new='Subhu is the thief asking for $500'
+pm=process_message(new)
+alldonewithflyingcolors(pm,new)
+
+#%% 
+new='Satapda is asking for the bride $500'
 pm=process_message(new)
 alldonewithflyingcolors(pm,new)
